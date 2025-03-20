8c7adf1003377c34d409243826b04f53a7244eb1
StrayerSQH <154659308+StrayerSQH@users.noreply.github.com>
2025-03-20 11:07:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const text = "Hello there! I'm StrayerSQH!";
+let index = 0;
+const textElement = document.getElementById('text');
 
+function displayText() {
+    if (index < text.length) {
+        textElement.textContent += text.charAt(index);
+        index++;
+        setTimeout(displayText, 100); // 每100毫秒显示一个字符
+    }
+}
+
+displayText();
